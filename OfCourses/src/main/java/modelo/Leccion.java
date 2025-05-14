3dc0463bd5e0d0de2aa1f54998f1fd57be251454
dancs334 <decosa43@gmail.com>
2025-05-14 23:15:01 +0200

    Fix: Atributos Calculados

--- OfCourses/src/main/java/modelo/Leccion.java
+++ OfCourses/src/main/java/modelo/Leccion.java
@@ -21,6 +21,7 @@ public class Leccion {
 	@JoinColumn(name = "leccion_id")
 	private List<Pregunta> preguntas;
 	
+	private int ultimaPregunta;
 	
 	private boolean completada;
 	
@@ -28,11 +29,13 @@ public class Leccion {
 	public Leccion () {}
 	
 	public Leccion(String titulo, String descripcion, List<Pregunta> preguntas) {
-				
+		
 		this.titulo = titulo;
 		this.descripcion = descripcion;
 		this.preguntas = new ArrayList<Pregunta>(preguntas);
 		this.completada = false;
+
+		this.ultimaPregunta = 0;
 	}
 
 	// Getters
@@ -55,9 +58,18 @@ public class Leccion {
 	public boolean getCompletada() {
 		return completada;
 	}
-
+	
+	public int getUltimaPregunta() {
+		return ultimaPregunta;
+	}
+	
 	// Setters
 	public void setCompletada(boolean completada) {
 		this.completada = completada;
 	}
+
+	public void setUltimaPregunta(int ultimaPregunta) {
+		this.ultimaPregunta = ultimaPregunta;
+	}
+	
 }
